6a8c2cba097352194526dafa9c8425a32aa43c24
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2024-03-05 19:35:01 +0000

    css

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,12 @@
 import react from "react";
+import "./Weather.css";
+
+
  export default function Weather(){
     return (
-        <div>
+        <div className="Weather">
             hello
+            
         </div>
     )
  } 
