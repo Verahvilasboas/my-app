c74fb302bc724f998c4a3ae8ad0bb9f3bfec72d0
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2024-03-05 20:05:08 +0000

    css added and html

--- src/Weather.js
+++ src/Weather.js
@@ -5,11 +5,42 @@ import "./Weather.css";
  export default function Weather(){
     return (
       <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="Search"
+                placeholder="Enter a city"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input className="btn btn-primary" type="submit" Search />
+            </div>
+          </div>
+        </form>
         <h2> New York </h2>
         <ul>
           <li>Wednesday 07:20</li>
           <li>Mostly cloudy</li>
         </ul>
+        <div className="row">
+          <div className="col-6">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/023/258/075/non_2x/weather-icon-cloudy-sky-icon-free-png.png"
+              alt="cloudy"
+              width="90px"
+            />{" "}
+            6°C/F
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Percipitation: 15 % </li>
+              <li> Humidity: 72% </li>
+              <li> Wind: 13 km/h </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
  } 
