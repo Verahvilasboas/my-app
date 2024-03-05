415e8a23c62b39fe63a352ffc06c03bb0bea9e80
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2024-03-05 19:07:19 +0000

    css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <button class="btn btn-primary" type="submit">
+          Button
+        </button>
+        <h1>Weather App</h1>
         <footer>
           This project was coded by{" "}
           <a
