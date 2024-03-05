ef58d5bc78104f4bd18d69645ffc8a976d6c6c07
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2024-03-05 19:40:53 +0000

    css addeed

--- src/Weather.js
+++ src/Weather.js
@@ -4,9 +4,12 @@ import "./Weather.css";
 
  export default function Weather(){
     return (
-        <div className="Weather">
-            hello
-            
-        </div>
-    )
+      <div className="Weather">
+        <h2> New York </h2>
+        <ul>
+          <li>Wednesday 07:20</li>
+          <li>Mostly cloudy</li>
+        </ul>
+      </div>
+    );
  } 
