c6b607e85c4cd85fa372607a939e263afddfb91a
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2024-03-09 20:07:32 +0000

    animated icon

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ const [city, setCity] = useState(props.defaultCity);
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
       date: new Date(response.data.dt * 1000),
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedData";
+import WeatherIcon from "./Weathericon";
 
 export default function Weatherinfo(props){
     return (
@@ -15,11 +16,10 @@ export default function Weatherinfo(props){
         <div className="row mt-3">
           <div className="col-6">
             <div className="clearfix ">
-              <img
-                src={props.data.iconUrl}
-                alt={props.data.description}
-                className="float-left"
-              />
+              <div className="float-left">
+                <WeatherIcon code={props.data.icon}
+                />
+              </div>
               <span className="temperature">
                 {" "}
                 {Math.round(props.data.temperature)}{" "}
